b066679ffb4eb1596134fa106ede83876c85d23c
dethdev00 <destinyjustice007@gmail.com>
2025-04-23 16:03:37 +0700

    update

--- app/Http/Controllers/PrintMopController.php
+++ app/Http/Controllers/PrintMopController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BhaiMorp;
+use App\Models\LogBhai;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class PrintMopController extends Controller
@@ -13,6 +14,14 @@ class PrintMopController extends Controller
         $data = BhaiMorp::find($id); 
         $data->del = 2;
         $data->save();
+
+        $log = new LogBhai();
+        $log->user_id = auth()->user()->id;
+        $log->bhaimops_id = $data->id;
+        $log->stt = 2;
+        $log->valuedt = date('Y-m-d H:i:s');
+        $log->save();
+
         $pdfs = mb_convert_encoding(\View::make('livewire.bill.mop.pdf', ['data'=>$data]), 'HTML-ENTITIES', 'UTF-8');
         return PDF::loadHtml($pdfs)->setPaper('A4', 'portrait')->stream('ໃບມອບເງິນສົດ.pdf',array('Attachment'=>0));
         
